700ae66cae34cc9fe2cdfdd16006368963fbf10d
Mary Bonilla <marybonillaseverino@gmail.com>
2023-09-25 23:45:00 +0200

    update

--- models/User.model.js
+++ models/User.model.js
@@ -1,11 +1,24 @@
 const mongoose = require ("mongoose");
 
+const EMAIL_PATTERN =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
 const UserSchema = new mongoose.Schema (
     {
         "username": {
             type: String,
             "require" : true,
         },
+        "surname": {
+            type: String,
+            "require" : true,
+        },
+        "email": {
+            type: String,
+            required: true,
+            unique: true,
+            match: EMAIL_PATTERN,
+          },
         "password": {
             type: String,
             "require" : true,
@@ -13,8 +26,13 @@ const UserSchema = new mongoose.Schema (
        
         "avatar": {
             type: String,
+            default : "https://cdn1.iconfinder.com/data/icons/web-seo-and-marketing/512/camera-1024.png"
         }
-    }
+    },
+    {
+        timestamps: true,
+        
+      }
 );
 
 const User = mongoose.model("User" , UserSchema);

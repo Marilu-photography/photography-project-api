9e77caf3c1d917ae2f3726a1487f9f120c27829d
Lu <jlu.fernandez90@gmail.com>
2023-09-26 00:03:50 +0200

    login y registro en postman ok

---
+++ controllers/auth.controller.js
@@ -0,0 +1,47 @@
+const User = require("../models/User.model");
+const { StatusCodes } = require("http-status-codes");
+const createError = require("http-errors");
+const jwt = require("jsonwebtoken");
+
+module.exports.register = (req, res, next) => {
+  if (req.file) {
+    req.body.avatar = req.file.path;
+  }
+
+  User.create(req.body)
+    .then((user) => res.status(StatusCodes.CREATED).json(user))
+    .catch(next);
+};
+
+module.exports.login = (req, res, next) => {
+  const loginError = createError(
+    StatusCodes.UNAUTHORIZED,
+    "Invalid email or password"
+  );
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(loginError);
+  }
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return next(loginError);
+      }
+      return user.checkPassword(password).then((match) => {
+        if (!match) {
+          return next(loginError);
+        }
+        const token = jwt.sign(
+          { id: user._id },
+          process.env.JWT_SECRET || "Super Secret",
+          {
+            expiresIn: "1h",
+          }
+        );
+        res.json({ accesToken: token });
+      });
+    })
+    .catch(next);
+};
--- models/User.model.js
+++ models/User.model.js
@@ -1,22 +1,79 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const UserSchema = new mongoose.Schema (
-    {
-        "username": {
+const SALT_WORK_FACTOR = 10;
+const EMAIL_PATTERN =
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: [true, "Username is required."],
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: [3, "Name needs at least 3 chars"],
+      maxlength: [50, "Name needs max 50 chars"],
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      lowercase: true,
+      match: [EMAIL_PATTERN, "Please use a valid email address."],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password needs at least 8 chars"],
+    },
+
+    avatar: {
             type: String,
-            "require" : true,
+            default: 'https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg'
         },
-        "password": {
-            type: String,
-            "require" : true,
-        },
-       
-        "avatar": {
-            type: String,
-        }
-    }
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret) => {
+        ret.id = doc.id;
+        delete ret._id;
+        delete ret.__v;
+        delete ret.password;
+        return ret;
+      },
+    },
+  }
 );
 
-const User = mongoose.model("User" , UserSchema);
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    bcrypt
+      .genSalt(SALT_WORK_FACTOR)
+      .then((salt) => {
+        return bcrypt.hash(user.password, salt)
+        .then((hash) => {
+          user.password = hash;
+          next();
+        });
+      })
+      .catch((error) => next(error));
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.checkPassword = function (passwordToCheck) {
+  return bcrypt.compare(passwordToCheck, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;

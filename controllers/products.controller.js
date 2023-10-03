fdd89e9fc4fbe5464714a31a375404f05c5e7cdd
Lu <jlu.fernandez90@gmail.com>
2023-10-03 21:55:00 +0200

    cart2

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,6 +1,7 @@
 const createHttpError = require('http-errors');
 const Product = require('../models/Products.model');
 const { StatusCodes } = require('http-status-codes');
+const stripe  = require('stripe')(process.env.STRIPE_SECRET_KEY);;
 
 
 module.exports.list = (req, res, next) => {
@@ -16,25 +17,29 @@ module.exports.list = (req, res, next) => {
       .catch(next)
   }
 
-  module.exports.createCheckoutSession = async  (req, res, next) => {
-    const { _id, name, price, image } = req.body;
+  module.exports.createCheckoutSession = async (req, res, next) => {
+    const { _id, price, name, description, image } = req.body;
   
-  const session = await stripe.checkout.sessions.create({
-    paymen_methof_types: ['card'],
-    line_items: [
-      {
-        price_data: {
-          currency: 'eur',
-          product_data: {
-            name: name,
-            images: [image]
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            currency: 'eur',
+            product_data: {
+              name: name,
+              description: description,
+              images: [image]
+            },
+            unit_amount: parseFloat((price * 100).toFixed(2)),
           },
-          unit_amount: parseFloat((price) * 100).toFixed(2),
+          quantity: 1,
         },
-        quantity: 1
-      },
-    ],
-    mode: 'payment',
-    succes_url: `${process.env.APP_HOST}/products/${_id}/success=true`, 
-    cancel_url: `${process.env.APP_HOST}/products/${_id}/canceled=true`, 
-  })}
+      ],
+      mode: 'payment',
+      success_url: `${process.env.APP_HOST}/products/${_id}?success=true`,
+      cancel_url: `${process.env.APP_HOST}/products/${_id}?canceled=true`,
+    });
+  
+    res.json({url: session.url});
+  }
